12a29e7b61418834335d84cf8ab6f18eaf729524
Orino1 <orinomohamed@gmail.com>
2023-06-06 12:18:13 -0700

    using format with prin

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
 
-
 for char in 'abcdefghijklmnopqrstuvwxyz':
-    print(char, end='')
+    print("{}".format(char), end='')
