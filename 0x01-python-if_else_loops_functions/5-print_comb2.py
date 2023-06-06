3c81d0174ee72e84c60cb5e421356b695d82c8b5
Orino1 <orinomohamed@gmail.com>
2023-06-06 13:42:13 -0700

    adding 5-print_comb2.py file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(100):
-    if i != 99:
-        print("{}, ".format(i), end='')
-    else:
-        print("{}".format(i))
+for i in range(10):
+    for j in range(10):
+        if i == 9 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end='')
