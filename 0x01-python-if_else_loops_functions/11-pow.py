b208125d6e609d66b2bda35c46b7bf2be20a280d
Orino1 <orinomohamed@gmail.com>
2023-06-06 16:31:18 -0700

    adding 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,7 +2,13 @@
 
 
 def pow(a, b):
-    result = 1
-    for i in range(b):
-        result *= a
-    return result
+    if b >= 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+        return result
+    else:
+        result = 1
+        for _ in range(-b):
+            result /= a
+        return result
