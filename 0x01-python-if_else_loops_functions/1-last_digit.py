be421642ed2d2eb8f567d453208d1f39b2128ca1
Orino1 <orinomohamed@gmail.com>
2023-06-06 12:07:23 -0700

    fixing the converter function

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,21 +3,19 @@ import random
 number = random.randint(-10000, 10000)
 
 
-def converter(number):
+def lastdegit(number):
     if number < 0:
-        return number * -1
-    return number
+        number = -(number)
+        return -(number%10)
+    return (number%10)
 
 
-if converter(number) % 10 == 0:
-    print(f"Last digit of {number} is {converter(number) % 10} and is 0")
-elif converter(number) % 10 > 5:
-    print(f"Last digit of {number} is {converter(number) % 10} and "
+if lastdegit(number) == 0:
+    print(f"Last digit of {number} is {lastdegit(number)} and is 0")
+elif lastdegit(number) > 5:
+    print(f"Last digit of {number} is {lastdegit(number)} and "
           f"is greater than 5")
-elif converter(number) % 10 < 6 and converter(number) % 10 != 0:
+elif lastdegit(number) < 6 and lastdegit(number) != 0:
     if number < 0:
-        print(f"Last digit of {number} is -{converter(number) % 10} and "
-              f"is less than 6 and not 0")
-    else:
-        print(f"Last digit of {number} is {converter(number) % 10} and "
+        print(f"Last digit of {number} is {lastdegit(number)} and "
               f"is less than 6 and not 0")
