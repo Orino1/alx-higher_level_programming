0f9e6eebf150dfc51cd03dbb03ec4e92f6cd4ece
Orino1 <orinomohamed@gmail.com>
2023-06-08 20:27:09 +0100

    Update: Adding the interpter that will run this script + code excute only if run directlly

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
-from add_0 import add
+#!/usr/bin/python3
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    from add_0 import add
 
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
