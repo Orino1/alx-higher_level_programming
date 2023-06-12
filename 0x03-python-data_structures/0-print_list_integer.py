467785580e5eaed6c7c0cf4f78b00bb6b3078510
Orino1 <orinomohamed@gmail.com>
2023-06-12 18:39:31 +0100

    Update: using the list its self in printf

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 
-def print_list_integer(my_list):
-    for num in my_list:
-        print('{}'.format(num))
+def print_list_integer(my_list=[]):
+    for i in range(my_list):
+        print('{}'.format(my_list[i]))
