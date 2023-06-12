62e1ada7885532d9a42c60e318deb82530368132
Orino1 <orinomohamed@gmail.com>
2023-06-12 19:54:42 +0100

    Update: pushed without even adding the code to the right file :(

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    big = None
+    if len(my_list) != 0:
+        big = my_list[0]
+        for i in my_list[1:]:
+            if i > big:
+                big = i
+    return big
