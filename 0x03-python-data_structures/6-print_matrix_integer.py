4aefc6461f94146a96f5f20525f227865344b678
Orino1 <orinomohamed@gmail.com>
2023-06-13 01:53:21 +0100

    Update: adding format spicifier :d

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@
 def print_matrix_integer(matrix=[[]]):
     for elem in matrix:
         for i in elem:
-            print("{}".format(i), end=' ' if i != elem[-1] else "")
+            print("{:d}".format(i), end=' ' if i != elem[-1] else "")
         print()
