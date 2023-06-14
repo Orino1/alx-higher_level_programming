5e417e2f7bee8ace4dca61dbe8475c7aaf09785d
Orino1 <orinomohamed@gmail.com>
2023-06-14 20:25:19 +0100

    Update: adding if statment to check if the key exist or not

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    result = a_dictionary.get(key)
+    if result == None:
+        return a_dictionary
     del a_dictionary[key]
-
     return a_dictionary
