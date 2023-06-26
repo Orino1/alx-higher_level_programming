808b625a0970fa6123b11b0545f085f6b3bab3e0
Orino1 <orinomohamed@gmail.com>
2023-06-26 04:28:39 +0100

    Update: Fixing the return value

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
+            num = num + my_list[i]
     except:
         pass
     print()
