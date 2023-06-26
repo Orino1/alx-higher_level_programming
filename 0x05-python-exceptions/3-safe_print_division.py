db675207fe5fc013e4bac2afca0c309cbcc24777
Orino1 <orinomohamed@gmail.com>
2023-06-26 20:47:21 +0100

    Update: Forgot the ain function inside th code

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -11,15 +11,3 @@ def safe_print_division(a, b):
         else:
             print("Inside result: None")
     return num
-
-
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
