d36b761319704f9110ba59daf4b68eb77ffb8611
Orino1 <orinomohamed@gmail.com>
2023-06-28 02:25:38 +0100

    Update: Fixing the logic of the function by using isinstance to chcek if size is int or not

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,8 @@ class Square:
     """a square that with a private size attribute"""
 
     def __init__(self, size=0):
-        try:
-            if size < 0:
-                raise ValueError("size must be an integer")
-            self.__size = size
-        except TypeError:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be an integer")
+        self.__size = size
