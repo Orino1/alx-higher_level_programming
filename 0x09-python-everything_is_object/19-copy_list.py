2532029166eb731006e6f5c2e3d84e37a56779d5
Orino1 <orinomohamed@gmail.com>
2023-07-04 19:06:21 +0100

    Update: No doucm needed and only 3 lines of code are allowed

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-"""
-function that return a copy of a list
-"""
 def copy_list(l):
-    """
-    this is the function
-    """
-    new_list = list(l)
-    return new_list
+    return list(l)
