52c9b368bb57101954dc5bbcd68b3d57d8dfb8fb
Orino1 <orinomohamed@gmail.com>
2023-07-13 03:10:35 +0100

    Update: Adding a more discriptive doc

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,7 +5,15 @@ This is a module for printing content from a file.
 """
 def append_write(filename="", text=""):
     """
-    function that appends a string at the end of a text file
+    This function append to a file
+    or creat it if dosnt exsist
+
+    Args:
+        filename (str): The name of the file.
+        text (str): the content of the file
+
+    Returns:
+        none
     """
     with open(filename, "a+", encoding="UTF8") as my_file:
         my_file.write(text)
