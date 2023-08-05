66d9df2d4191c4f2feba667e1f31e38679cc4887
Orino1 <orinomohamed@gmail.com>
2023-08-05 23:12:41 +0100

    Update: refactoring the code + correcting pip8 style

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+"""A module that contain a square class """
 
-"""defining the class"""
 
 class Square:
-    """a square that with a private size attribute"""
+
+    """Square class: empty at the moment"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        """inistialazation of square object with an optional private attribute
+        size, while checking if size is int/and if greater than 0,and raising
+        errors accordinlly"""
+        if isinstance(size, int):
+            if size > 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
             raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
