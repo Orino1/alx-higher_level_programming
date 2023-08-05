fc2b8a74dbc129a16d011c791e4c3a7b7998a72d
Orino1 <orinomohamed@gmail.com>
2023-08-05 23:58:27 +0100

    Update: refactoring the code + correcting pip8 style

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-"""defining the class"""
+"""A module that contain a square class """
+
 
 class Square:
-    """a square that with a private size attribute"""
+
+    """Square class: with init method and a public method area
+    and the following attributes:
+        __size : size of the square (private)"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        """inistialazation of square object with an optional private attribute
+        size, while checking if size is int/and if greater than 0,and raising
+        errors accordinlly"""
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
             raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-    """method: def area(self): that returns the current square area"""
 
     def area(self):
-        return self.__size * self.__size
+        """area is a public method
+        args: None (other than the object it self)
+        return value: the size attribute of the instance square"""
+        return self.__size
