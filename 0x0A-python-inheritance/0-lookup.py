61a8d29fa7cf47892f7362dcbcf03531fefcb337
Orino1 <orinomohamed@gmail.com>
2023-08-11 00:36:31 +0100

    Update: Adding doc for lookup function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """
-an object attribute lookup function.
+an object/class attribute lookup function.
 """
 
 
 def lookup(obj):
+    """
+    returns all attribute of an object/class.
+    Args:
+        obj(object/class): the object or the class.
+    Return:
+        all attribute in a list.
+    """
     return (dir(obj))
