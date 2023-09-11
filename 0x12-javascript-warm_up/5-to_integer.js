615e7007c12b0499abe1b3b1b10e1da482fde157
Orino1 <orinomohamed@gmail.com>
2023-09-11 23:11:00 +0100

    Update: LOL i was not even allowed to use trycatch block and i reqd it mistakenlly

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
-try {
-  if (process.argv[2] === undefined) {
-    throw new Error();
-  }
-  const mynumber = parseInt(process.argv[2]);
-  if (isNaN(mynumber)) {
-    throw new Error();
-  }
-  console.log('My number: ' + mynumber);
-} catch {
+const mynumber = parseInt(process.argv[2]);
+if (isNaN(mynumber)) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + mynumber);
 }
