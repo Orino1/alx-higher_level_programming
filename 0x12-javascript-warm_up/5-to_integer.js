16412fea85c868ba0cac131b11d9e1f7349d1281
Orino1 <orinomohamed@gmail.com>
2023-09-11 23:02:23 +0100

    Adding 5-to_integer.js File

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+try {
+    if (process.argv[2] === undefined) {
+        throw new Error();
+    }
+    const mynumber = parseInt(process.argv[2]);
+    if (isNaN(mynumber)) {
+        throw new Error();
+    }
+    console.log('My number: ' + mynumber);
+} catch {
+    console.log('Not a number');
+}
