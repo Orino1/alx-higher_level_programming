a8f02f9778a693aaf619edf92a4f4e9f29bdd411
Orino1 <orinomohamed@gmail.com>
2023-09-16 19:54:46 +0100

    Adding 3-my_safe_filter_states.py File

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Retrieve and print a list of states from a MySQL database.
+Usage: script.py <username> <password> <database_name> <state_name_searched>
+Also: its safe from MySQL injections.
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    """
+    Connect to a MySQL database and print a the id and name
+    of a state that matche the searched name.
+    Usage: script.py <username> <password> <database_name>
+    <state_name_searched>
+    """
+    connection = MySQLdb.connect(
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                host='localhost',
+                port=3306,
+                db=sys.argv[3]
+            )
+    state_name = sys.argv[4]
+    cursor = connection.cursor()
+    query = "SELECT id, name FROM states WHERE name=%s"
+    cursor.execute(query, (state_name,))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+
+if __name__ == "__main__":
+    main()
