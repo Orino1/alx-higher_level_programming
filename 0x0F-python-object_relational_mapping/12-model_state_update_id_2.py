6f8f4f2b78896066749c739cef67601a94fa0813
Orino1 <orinomohamed@gmail.com>
2023-09-17 21:49:09 +0100

    Update: i coded the task in the script n 11 by mistake

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    state_to_edit = ses.query(State).filter(State.id == 2).first()
+    state_to_edit.name = "New Mexico"
+    ses.commite()
