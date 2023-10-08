f95381ac7b4d766e8a488d41d883905fdeca2bb3
Orino1 <orinomohamed@gmail.com>
2023-10-08 07:53:55 +0100

    Update: adding Double quote around teh variable

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-curl -sI $1 | sed -n 's/Content-Length: //p'
+curl -sI "$1" | sed -n 's/Content-Length: //p'
+
