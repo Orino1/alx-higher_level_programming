def07f6ab08e9ffd893610162c727943b390d9a5
Orino1 <orinomohamed@gmail.com>
2023-10-09 05:48:05 +0100

    Update: the script wont excu when imported

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,9 @@
 """fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
-url = "https://alx-intranet.hbtn.io/status"
-response = requests.get(url)
-print("Body response:")
-print(f"\t- type: {type(response)}")
-print(f"\t-- content: {type(response.text)}")
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print(f"\t- type: {type(response)}")
+    print(f"\t-- content: {type(response.text)}")
