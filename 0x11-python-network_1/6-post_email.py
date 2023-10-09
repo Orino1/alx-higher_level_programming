6003152becccfa47e191ee28ddbc68eaebf3d13b
Orino1 <orinomohamed@gmail.com>
2023-10-09 06:25:30 +0100

    Adding 6-post_email.py File

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""script that takes in a URL and an email address, sends
-a POST request to the passed URL with the email as a parameter,
-and finally displays the body of the response."""
+"""
+takes in a URL and an email address
+sends a POST request to the passed URL with the email as a parameter
+and finally displays the body of the response.
+"""
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    print(requests.post(argv[1], data={'email': argv[2]}).text)
+    url = sys.argv[1]
+    header = f"email: {sys.argv[2]}"
+    response = requests.post(url, headers=header)
+    print(response.text)
