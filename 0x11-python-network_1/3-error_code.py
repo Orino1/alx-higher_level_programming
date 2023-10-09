ca32c9d3d08b53036061fd576fad0e4d59fae6c0
Orino1 <orinomohamed@gmail.com>
2023-10-09 05:30:53 +0100

    Adding 3-error_code.py File

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""script that takes in a URL, sends a request to the URL
-and displays the body of the response (decoded in utf-8)."""
-from urllib import request, error
-from sys import argv
+"""
+takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
+import urllib.request
+import urllib.error
+import sys
 
 if __name__ == "__main__":
+    url = sys.argv[1]
     try:
-        with request.urlopen(argv[1]) as page:
-            print(page.read().decode('utf-8'))
-    except error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as error:
+        print(f"Error code: {error.code}")
