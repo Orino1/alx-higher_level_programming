b73243fea0de0ae6c21d3df08463aadc6ca18881
Orino1 <orinomohamed@gmail.com>
2023-10-09 02:45:07 +0100

    Update: preventing the model from excu when its imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Fetch and display data from a URL."""
+"""Fetch and display data from a URL.."""
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
+
 if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
     print("Body response:")
