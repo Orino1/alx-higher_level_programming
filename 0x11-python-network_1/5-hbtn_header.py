b631a9f4fa48fc95b51cf8018f5aabfbc6974d4a
Orino1 <orinomohamed@gmail.com>
2023-10-09 06:03:26 +0100

    Update: the script wont be excu when imported

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,5 +6,6 @@ X-Request-Id in the response header
 import requests
 import sys
 
-response = requests.get(sys.argv[1])
-print(response.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    print(response.headers.get("X-Request-Id"))
