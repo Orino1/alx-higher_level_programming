811d990332dd13272be43257df2968e993003374
Orino1 <orinomohamed@gmail.com>
2023-10-09 05:52:17 +0100

    Update: printing the text at the end instead of the type

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,8 @@
 import requests
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
+    url = "http://orino.tech"
     response = requests.get(url)
     print("Body response:")
     print(f"\t- type: {type(response)}")
-    print(f"\t-- content: {type(response.text)}")
+    print(f"\t-- content: {response.text}")
