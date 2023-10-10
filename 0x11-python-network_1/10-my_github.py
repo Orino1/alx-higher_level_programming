a87dd75dfde6c17f60f25d2aefb1ba5ad1b1faca
Orino1 <orinomohamed@gmail.com>
2023-10-10 03:58:25 +0100

    Adding 10-my_github.py File

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-"""script that takes GitHub credentials (username and password)
-and uses the GitHub API to display the user id."""
+"""
+takes your GitHub credentials (username and token)
+and uses the GitHub API to display your id.
+"""
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    auth = (argv[1], argv[2])
-    request = requests.get("https://api.github.com/user", auth=auth)
-    print(request.json().get("id"))
+    user = sys.argv[1]
+    token = sys.argv[2]
+    url = f"https://api.github.com/users/{user}"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    with requests.get(url, headers=headers) as response:
+        data = response.json()
+        print(data.get("id"))
